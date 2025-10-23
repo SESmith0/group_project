ffb19f405639c9515f125c12213d359745b13ff7
mthorpecsc <mthorpecsc@gmail.com>
2025-10-23 18:50:11 -0400

    Problem 4!

--- problem4.py
+++ problem4.py
@@ -1,1 +1,24 @@
+def fizzbuzz(i):
 
+    if i % 3 == 0:
+    #determines if the given integer is divisbible by 3
+        print("Fizz")
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    #determines if the given integer is divisble by 5 AND 3
+    if i % 5 == 0:
+        print("Buzz")
+    #determines if the given integer is disvisble by 5
+    if i % 3 == 1 or i % 5 == 1:
+        print(i)
+
+    
+
+fizzbuzz(15)
+#should print fizz, fizzbuzz, and buzz
+fizzbuzz(1)
+#should print out 1
+fizzbuzz(3)
+#should print out fizz
+
+#i did not check for any other possible edgecases
